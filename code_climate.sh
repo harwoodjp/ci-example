149170af69968c012b5ca7df43aeb7963645e9f8
Justin Harwood <justin@harwoodjp.com>
2022-08-24 18:11:39 -0500

    init

--- code_climate.sh
+++ code_climate.sh
@@ -6,4 +6,3 @@ pip3 install -r requirements.txt
 ./cc-test-reporter before-build
 pytest --cov=. --cov-report=xml --cov-report=term app.py
 ./cc-test-reporter after-build
-./cc-test-reporter upload-coverage
