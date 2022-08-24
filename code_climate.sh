430e1d5f355ce10d0b29a34788c4922d33c00fc1
Justin Harwood <justin@harwoodjp.com>
2022-08-24 18:17:34 -0500

    cat coverage.xml

--- code_climate.sh
+++ code_climate.sh
@@ -5,5 +5,5 @@ chmod +x cc-test-reporter
 pip3 install -r requirements.txt
 ./cc-test-reporter before-build
 pytest --cov=. --cov-report=xml --cov-report=term app.py
+cat coverage.xml
 ./cc-test-reporter after-build
-./cc-test-reporter upload-coverage
