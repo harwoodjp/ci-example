cc1608ac4d2e33dbacd4c0a6f3a2b4baa17888b6
Justin Harwood <justin@harwoodjp.com>
2022-08-24 18:09:37 -0500

    add upload-coverage to gha

--- code_climate.sh
+++ code_climate.sh
@@ -6,3 +6,4 @@ pip3 install -r requirements.txt
 ./cc-test-reporter before-build
 pytest --cov=. --cov-report=xml --cov-report=term app.py
 ./cc-test-reporter after-build
+./cc-test-reporter upload-coverage
