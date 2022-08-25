26d4c53e27fbad17467efbf1e7b7b58f0fa7b7e5
Justin Harwood <justin@harwoodjp.com>
2022-08-25 15:06:26 -0500

    debug7

--- code_climate.sh
+++ code_climate.sh
@@ -3,15 +3,12 @@ echo "GITHUB_SHA: ${GITHUB_SHA}"
 echo "GITHUB_REF_NAME: ${GITHUB_REF_NAME}"
 echo "GIT_BRANCH: ${GIT_BRANCH}"
 
-pwd
-ls -al
-
-# apt-get update -y
-# apt-get install curl git gpg -y
-# curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
-# chmod +x cc-test-reporter
-# pip3 install -r requirements.txt
-# ./cc-test-reporter before-build
-# pytest --cov=. --cov-report=xml --cov-report=term app.py
-# cat coverage.xml
-# ./cc-test-reporter after-build
+apt-get update -y
+apt-get install curl git gpg -y
+curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
+chmod +x cc-test-reporter
+pip3 install -r requirements.txt
+./cc-test-reporter before-build
+pytest --cov=. --cov-report=xml --cov-report=term app.py
+cat coverage.xml
+./cc-test-reporter after-build
