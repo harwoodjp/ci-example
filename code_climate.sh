13ce1748906012e09ef07f69003c976d35f78017
Justin Harwood <justin@harwoodjp.com>
2022-08-26 12:47:59 -0500

    upd code_climate.sh

--- code_climate.sh
+++ code_climate.sh
@@ -1,14 +1,24 @@
-echo "CC_TEST_REPORTER_ID: ${CC_TEST_REPORTER_ID}"
-echo "GITHUB_SHA: ${GITHUB_SHA}"
-echo "GITHUB_REF_NAME: ${GITHUB_REF_NAME}"
-echo "GIT_BRANCH: ${GIT_BRANCH}"
-
 apt-get update -y
 apt-get install curl git gpg -y
-curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
-chmod +x cc-test-reporter
+
+curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 -o test-reporter-latest-linux-amd64 
+curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64.sha256 -o test-reporter-latest-linux-amd64.sha256
+curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64.sha256.sig -o test-reporter-latest-linux-amd64.sha256.sig
+
+if ! grep test-reporter-latest-linux-amd64 test-reporter-latest-linux-amd64.sha256 | shasum -a 256 -c - | grep "OK"
+then 
+   exit
+fi
+
+gpg --keyserver  keys.openpgp.org --recv-keys 9BD9E2DD46DA965A537E5B0A5CBF320243B6FD85
+if ! gpg --verify test-reporter-latest-linux-amd64.sha256.sig test-reporter-latest-linux-amd64.sha256
+then
+   exit
+fi
+
+chmod +x test-reporter-latest-linux-amd64
 pip3 install -r requirements.txt
-./cc-test-reporter before-build
+./test-reporter-latest-linux-amd64 before-build
 pytest --cov=. --cov-report=xml --cov-report=term app.py
 cat coverage.xml
-./cc-test-reporter after-build
+./test-reporter-latest-linux-amd64 after-build
