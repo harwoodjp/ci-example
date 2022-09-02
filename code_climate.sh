f01099887f4259cb0eeb23f1b6664c4936ec5a98
Justin Harwood <justin@harwoodjp.com>
2022-09-02 12:51:46 -0500

    pytest -x

--- code_climate.sh
+++ code_climate.sh
@@ -19,6 +19,6 @@ fi
 chmod +x test-reporter-latest-linux-amd64
 pip3 install -r requirements.txt
 ./test-reporter-latest-linux-amd64 before-build
-pytest --cov=. --cov-report=xml --cov-report=term app.py
+pytest -x --cov=. --cov-report=xml --cov-report=term app.py
 cat coverage.xml
 ./test-reporter-latest-linux-amd64 after-build
